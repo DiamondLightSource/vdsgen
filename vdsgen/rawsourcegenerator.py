1f9665a1975c48a01bf401781b17840cc79ccd2c
Gary Yendell <gary.yendell@diamond.ac.uk>
2018-05-22 09:50:54 +0100

    Add raw HDF5 file generator to test FrameVDSGenerator against

---
+++ vdsgen/rawsourcegenerator.py
@@ -0,0 +1,52 @@
+#!/bin/env dls-python
+from pkg_resources import require
+require("numpy")
+require("h5py")
+
+import numpy as np
+import h5py
+
+import sys
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+
+def parse_args():
+    """Parse command line arguments."""
+    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        "prefix", type=str, help="Path and name prefix")
+    parser.add_argument(
+        "frames", type=int, help="Number of frames.")
+    parser.add_argument(
+        "files", type=int, help="Number of files to spread frames across.")
+    parser.add_argument(
+        "block_size", type=int, default=1,
+        help="Size of contiguous blocks of frames.")
+
+    return parser.parse_args()
+
+
+def main():
+    """Run program."""
+    args = parse_args()
+
+    values = []
+    for _ in range(args.files):
+        values.append(list())
+
+    for frame_idx in range(args.frames):
+        block_idx = (frame_idx // (args.files - 1))
+        file_idx = block_idx % args.files
+        values[file_idx].append(frame_idx + 1)
+
+    for file_idx, file_values in enumerate(values):
+        blocks = []
+        for value in file_values:
+            blocks.append(np.full((100, 100), value))
+
+        with h5py.File(args.prefix + "_{}.h5".format(file_idx)) as f:
+            f.create_dataset("data", data=np.stack(blocks))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
